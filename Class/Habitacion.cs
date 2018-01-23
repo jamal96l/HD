928e258899d4fd144bd80854b58c5540298c1da5
Juan Pablo Altamirano <cincottel@hotmail.com>
2018-01-22 19:35:19 -0600

    Tercer Commit

--- Class/Habitacion.cs
+++ Class/Habitacion.cs
@@ -8,10 +8,40 @@ namespace HotelDesktop.Class
 {
     class Habitacion
     {
-        private int ID_Habitacion { get; set; }
-        private int ID_Estado { get; set; }
-        private int ExtensionTelefonica { get; set; }
-        private int ID_Tipo { get; set; }
-        private int ID_Hotel { get; set; }
+        private int _IdHabitacion;
+        private int _IdEstado;
+        private int _ExtensionTelefonica;
+        private int _IdTipo;
+        private int _IdHotel;
+
+        public int IdHabitacion
+        {
+            get { return _IdHabitacion; }
+            set { _IdHabitacion = value; }
+        }
+
+        public int IdEstado
+        {
+            get { return _IdEstado; }
+            set { _IdEstado = value; }
+        }
+
+        public int ExtensionTelefonica
+        {
+            get { return _ExtensionTelefonica; }
+            set { _ExtensionTelefonica = value; }
+        }
+
+        public int IdTipo
+        {
+            get { return _IdTipo; }
+            set { _IdTipo = value; }
+        }
+
+        public int IdHotel
+        {
+            get { return _IdHotel; }
+            set { _IdHotel = value; }
+        }
     }
 }
